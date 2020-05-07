f260ea957c6d99a97cdba97120dbb0c1f2a20c82
João Moura <joao.moura@b-i.com>
2020-05-07 23:37:32 +0100

    Linted project

--- src/index.ts
+++ src/index.ts
@@ -1,196 +1,194 @@
-import path from "path";
-import fs, { readSync } from "fs";
-import readline from "readline";
+import fs from 'fs'
+import readline from 'readline'
 
 interface IColRef {
-  table: string;
-  col: string;
+  table: string
+  col: string
 }
 
 interface ITableColumn {
-  name: string;
-  isPk?: boolean;
-  "NOT NULL"?: boolean;
-  ref?: IColRef;
-  type?: string;
-  default?: string;
-  unique?: boolean;
-  AUTO_INCREMENT?: boolean;
+  name: string
+  isPk?: boolean
+  'NOT NULL'?: boolean
+  ref?: IColRef
+  type?: string
+  default?: string
+  unique?: boolean
+  AUTO_INCREMENT?: boolean
 }
 
 interface ITableObject {
-  tableName: string;
-  pkList: string[];
+  tableName: string
+  pkList: string[]
   columns: {
     [key: string]: ITableColumn;
-  };
+  }
 }
 
 interface IUML {
-  [key: string]: ITableObject;
+  [key: string]: ITableObject
 }
 
 function getType(ref: IColRef, uml: IUML) {
-  return uml[ref.table].columns[ref.col].type;
+  return uml[ref.table].columns[ref.col].type
 }
 
 function getDefaultValue(columnData: ITableColumn) {
-  const columnType = columnData.type;
+  const columnType = columnData.type
   return columnData.default
     ? columnType &&
-        (((columnType.indexOf("INT") >= 0 ||
-          columnType.startsWith("DOUBLE") ||
-          columnType.startsWith("FLOAT") ||
-          columnType.startsWith("DECIMAL") ||
-          columnType === "DATE" ||
-          columnType === "TIMESTAMP") &&
-          " DEFAULT " + columnData.default) ||
-          (columnType === "BOOLEAN" &&
-            " DEFAULT " + (columnData.default === "true" ? 1 : 0)) ||
+        (((columnType.indexOf('INT') >= 0 ||
+          columnType.startsWith('DOUBLE') ||
+          columnType.startsWith('FLOAT') ||
+          columnType.startsWith('DECIMAL') ||
+          columnType === 'DATE' ||
+          columnType === 'TIMESTAMP') &&
+          ' DEFAULT ' + columnData.default) ||
+          (columnType === 'BOOLEAN' &&
+            ' DEFAULT ' + (columnData.default === 'true' ? 1 : 0)) ||
           " DEFAULT '" + columnData.default + "'")
-    : "";
+    : ''
 }
 
-function tryConvertToMySQL(uml: IUML) {
-  let createStatement = "";
-  const tables = Object.keys(uml);
+function UMLToMySQL(uml: IUML) {
+  let createStatement = ''
+  const tables = Object.keys(uml)
   tables.forEach((tableName) => {
-    const foreignKeys: string[] = [];
-    const primaryKeys: string[] = [];
-    const uniqueIndexes: string[] = [];
-    const columnLines: string[] = [];
-    createStatement += `\nCREATE TABLE IF NOT EXISTS ${tableName} (`;
-    const columns = uml[tableName].columns;
-    const columnKeys = Object.keys(uml[tableName].columns);
+    const foreignKeys: string[] = []
+    const primaryKeys: string[] = []
+    const uniqueIndexes: string[] = []
+    const columnLines: string[] = []
+    createStatement += `\nCREATE TABLE IF NOT EXISTS ${tableName} (`
+    const columns = uml[tableName].columns
+    const columnKeys = Object.keys(uml[tableName].columns)
     columnKeys.forEach((columnName) => {
-      const columnData = columns[columnName];
+      const columnData = columns[columnName]
       if (columnData.ref) {
-        columnData.type = getType(columnData.ref, uml);
+        columnData.type = getType(columnData.ref, uml)
         foreignKeys.push(
           `FOREIGN KEY (${columnData.name}) REFERENCES ${columnData.ref.table}(${columnData.ref.col})`
-        );
+        )
       }
       if (columnData.isPk) {
-        primaryKeys.push(columnName);
+        primaryKeys.push(columnName)
       }
       if (columnData.unique) {
         uniqueIndexes.push(
           `UNIQUE KEY \`idx_${tableName}_${columnName}\` (${columnName})`
-        );
+        )
       }
       columnLines.push(
         `${columnName} ${columnData.type}${getDefaultValue(columnData)}${
-          (columnData.AUTO_INCREMENT && " AUTO_INCREMENT") || ""
+          (columnData.AUTO_INCREMENT && ' AUTO_INCREMENT') || ''
         }`
-      );
-    });
+      )
+    })
 
-    createStatement += `\n${columnLines.join(",\n")}`;
+    createStatement += `\n${columnLines.join(',\n')}`
 
     if (primaryKeys.length) {
-      createStatement += `,\nPRIMARY KEY (${primaryKeys.join(",")})`;
+      createStatement += `,\nPRIMARY KEY (${primaryKeys.join(',')})`
     }
     if (foreignKeys.length) {
-      createStatement += `,\n${foreignKeys.join(",\n")}`;
+      createStatement += `,\n${foreignKeys.join(',\n')}`
     }
     if (uniqueIndexes.length) {
-      createStatement += `,\n${uniqueIndexes.join(",\n")}`;
+      createStatement += `,\n${uniqueIndexes.join(',\n')}`
     }
-    createStatement += `\n)  ENGINE=INNODB;\n`;
-  });
+    createStatement += `\n)  ENGINE=INNODB;\n`
+  })
 
-  return createStatement;
+  return createStatement
 }
 
 export default function parseFile(filePath: string) {
   const convertPlantumlToUML = new Promise<IUML>((res, rej) => {
-    let isUML = false;
-    let isTable = false;
-    let currentTableObject: ITableObject;
-    let currentColumn: ITableColumn;
-    let JSONUML: IUML = {};
+    let isUML = false
+    let isTable = false
+    let currentTableObject: ITableObject
+    let currentColumn: ITableColumn
+    const JSONUML: IUML = {}
 
     const rl = readline.createInterface({
       input: fs.createReadStream(filePath),
-    });
+    })
 
-    rl.on("line", (input) => {
-      if (!isUML && input.startsWith("@startuml")) {
-        isUML = true;
-        return;
+    rl.on('line', (input) => {
+      if (!isUML && input.startsWith('@startuml')) {
+        isUML = true
+        return
       }
       if (isUML) {
-        input = input.trim();
+        input = input.trim()
 
-        if (!isTable && input.startsWith("class")) {
-          isTable = true;
-          const tableName = input.split(" ")[1];
+        if (!isTable && input.startsWith('class')) {
+          isTable = true
+          const tableName = input.split(' ')[1]
           currentTableObject = {
             pkList: [],
             tableName,
             columns: {},
-          };
-          JSONUML[tableName] = currentTableObject;
-          return;
+          }
+          JSONUML[tableName] = currentTableObject
+          return
         }
 
-        if (isTable && input !== "}") {
-          let tableColData = input.split(" ");
+        if (isTable && input !== '}') {
+          const tableColData = input.split(' ')
           tableColData.forEach((colData, index) => {
-            let colName = colData;
+            let colName = colData
             if (index === 0) {
-              if (colData[0] === "#" || colData[0] === "+") {
-                colName = colData.substr(1);
-                currentTableObject.pkList.push(colName);
+              if (colData[0] === '#' || colData[0] === '+') {
+                colName = colData.substr(1)
+                currentTableObject.pkList.push(colName)
                 currentColumn = {
                   name: colName,
                   isPk: true,
-                };
-              } else if (colData === "..") {
-                return;
+                }
+              } else if (colData === '..') {
+                return
               } else {
-                if (colData[0] === "-") {
-                  colName = colData.substr(1);
+                if (colData[0] === '-') {
+                  colName = colData.substr(1)
                 }
                 currentColumn = {
                   name: colName,
                   isPk: false,
-                };
+                }
               }
-              currentTableObject.columns[colName] = currentColumn;
-            } else if (colData === "NN") {
-              currentColumn["NOT NULL"] = true;
-            } else if (colData === "AUTO_INCREMENT") {
-              currentColumn.AUTO_INCREMENT = true;
-            } else if (colData === "UNIQUE") {
-              currentColumn.unique = true;
-            } else if (colData.startsWith("REF(")) {
-              const ref = colData.slice(4, -1).split(".");
+              currentTableObject.columns[colName] = currentColumn
+            } else if (colData === 'NN') {
+              currentColumn['NOT NULL'] = true
+            } else if (colData === 'AUTO_INCREMENT') {
+              currentColumn.AUTO_INCREMENT = true
+            } else if (colData === 'UNIQUE') {
+              currentColumn.unique = true
+            } else if (colData.startsWith('REF(')) {
+              const ref = colData.slice(4, -1).split('.')
               currentColumn.ref = {
                 table: ref[0].charAt(0).toUpperCase() + ref[0].slice(1),
                 col: ref[1],
-              };
-            } else if (colData.startsWith("DEFAULT(")) {
-              currentColumn.default = colData.slice(8, -1);
+              }
+            } else if (colData.startsWith('DEFAULT(')) {
+              currentColumn.default = colData.slice(8, -1)
             } else {
-              currentColumn.type = colData;
+              currentColumn.type = colData
             }
-          });
-        } else if (isTable && input === "}") {
-          isTable = false;
+          })
+        } else if (isTable && input === '}') {
+          isTable = false
         }
       }
-    });
+    })
 
-    rl.on("error", (reason) => {
-      console.log('Error: ', reason);
-      return rej(reason);
-    });
-    
-    rl.on("close", () => {
-      res(JSONUML);
-    });
-  });
+    rl.on('error', (reason) => {
+      return rej(reason)
+    })
 
-  return convertPlantumlToUML.then((uml) => tryConvertToMySQL(uml));
+    rl.on('close', () => {
+      res(JSONUML)
+    })
+  })
+
+  return convertPlantumlToUML.then((uml) => UMLToMySQL(uml))
 }
